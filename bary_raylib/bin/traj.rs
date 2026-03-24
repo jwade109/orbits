a88a1fef09bb9280d13d4e26199f5569be2c64fb
Wade Foster <jwade109@vt.edu>
2026-03-23 22:46:53 -0400

    Use command queues in trajectory sim

--- bary_raylib/bin/traj.rs
+++ bary_raylib/bin/traj.rs
@@ -3,34 +3,61 @@ use std::io::Write;
 
 use bary_core::prelude::*;
 use bary_raylib::{
-    sim::systems::{TICKS_PER_SECOND, find, get_thruster_levels},
-    sim::world::update_world,
+    ops,
+    query::primary_computer_id,
+    sim::{
+        TimedInstruction,
+        systems::{TICKS_PER_SECOND, find, get_thruster_levels},
+        world::update_world,
+    },
     world_builder::WorldBuilder,
 };
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct SimSpec {
+    ship_name: String,
+    instructions: Vec<TimedInstruction>,
+    steps: u64,
+    secs_per_step: f32,
+}
+
+fn load_sim_file(filename: &str) -> Result<SimSpec, Box<dyn std::error::Error>> {
+    let s = std::fs::read_to_string(filename)?;
+    let spec: SimSpec = serde_yaml::from_str(&s)?;
+    Ok(spec)
+}
 
 struct SimEpoch {
     ticks: u64,
     pose: Isometry2d,
     vel: Isometry2d,
-    target: Isometry2d,
+    target: Option<Isometry2d>,
     acc_updates: u64,
     thrusters_firing: u32,
 }
 
 fn run_simulation(
     vehicle_name: &str,
-    target: Isometry2d,
-    steps: usize,
+    instructions: Vec<TimedInstruction>,
+    steps: u64,
     secs_per_step: f32,
 ) -> Vec<SimEpoch> {
     let mut world = WorldBuilder::new()
         .assets()
         .blueprint(vehicle_name)
         .spawn(vehicle_name, (0.0, 0.0, 0.0))
-        .waypoint(vehicle_name, target)
         .build();
 
     let grid_id = find::grid_by_name(&world.grids, vehicle_name).unwrap();
+
+    _ = ops::set_primary_computer_state(grid_id, true, &mut world);
+
+    let cpu_id = find::primary_computer_id(grid_id, &world.grids).unwrap();
+
+    let cpu = world.computers.try_get_mut(cpu_id).unwrap();
+
+    cpu.command_queue = instructions;
 
     let mut ret = Vec::new();
 
@@ -40,6 +67,8 @@ fn run_simulation(
             update_world(&mut world);
         }
 
+        let cpu_id = primary_computer_id(grid_id, &world.grids).unwrap();
+        let cpu = world.computers.try_get(cpu_id).unwrap();
         let pose = find::grid_pose(&world.grids, grid_id).unwrap();
         let vel = find::grid_vel(&world.grids, grid_id).unwrap();
 
@@ -51,7 +80,7 @@ fn run_simulation(
             ticks: world.ticks,
             pose,
             vel,
-            target,
+            target: cpu.current_waypoint(),
             acc_updates: world.grid_acceleration_updates,
             thrusters_firing,
         };
@@ -65,14 +94,16 @@ fn run_simulation(
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<_> = std::env::args().collect();
 
-    let ship_name = args.get(1).unwrap();
-    let x: f32 = args.get(2).unwrap().parse().unwrap();
-    let y: f32 = args.get(3).unwrap().parse().unwrap();
-    let r: f32 = args.get(4).unwrap().parse().unwrap();
+    let filename = args.get(1).unwrap();
 
-    let waypoint = Isometry2d::new((x, y).into(), r);
+    let spec = load_sim_file(filename).unwrap();
 
-    let epochs = run_simulation(ship_name, waypoint, 1000, 0.1);
+    let epochs = run_simulation(
+        &spec.ship_name,
+        spec.instructions.clone(),
+        spec.steps,
+        spec.secs_per_step,
+    );
     let mut file = File::create("sim.csv").unwrap();
 
     write!(file, "ticks,x,y,a,vx,vy,va,tx,ty,ta,updates,thrusters\n")?;
@@ -88,9 +119,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             epoch.vel.translation.x,
             epoch.vel.translation.y,
             epoch.vel.rotation,
-            epoch.target.translation.x,
-            epoch.target.translation.y,
-            epoch.target.rotation,
+            epoch
+                .target
+                .map(|t| t.translation.x)
+                .unwrap_or(std::f32::NAN),
+            epoch
+                .target
+                .map(|t| t.translation.y)
+                .unwrap_or(std::f32::NAN),
+            epoch.target.map(|t| t.rotation).unwrap_or(std::f32::NAN),
             epoch.acc_updates,
             epoch.thrusters_firing,
         )?;
